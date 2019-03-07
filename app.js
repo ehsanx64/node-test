f9a1e46c767428365c4fc911768240bdff83c913
Ehsan Mohammadi <ehsanx64@gmail.com>
2019-03-07 15:45:17 +0330

    Added /public dir as a static web directory

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/about', (req, res) => res.send('This is the about content for get'))
 app.post('/about', (req, res) => res.send('This is the about content for post'))
