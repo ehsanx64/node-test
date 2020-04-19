8dbb4eff50c39b586937e079717f5b45866d5511
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-04-19 10:45:16 +0430

    Added nodemailer test

---
+++ nodemailer/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('dotenv').config();
+console.log(process.env.SMTP_IP);
+const nodemailer = require('nodemailer');
+
+async function main() {
+    var transporter = nodemailer.createTransport({
+        pool: true,
+        host: process.env.SMTP_IP,
+        port: process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+            user: process.env.SMTP_USERNAME,
+            pass: process.env.SMTP_PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    var message = {
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_TO,
+        subject: 'Test',
+        text: 'The email text sent at: ' + new Date(),
+    };
+
+    var info = transporter.sendMail(message);
+}
+
+main().catch(err => {
+    console.error(err.message);
+    process.exit(1);
+});
